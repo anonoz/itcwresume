ae09457d6d084f16a6fa4bf45090c2d64d84a008
Anonoz Chong <honsiongchs@gmail.com>
2016-11-11 15:45:12 +0800

    WIP to bring home

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :employers
   root 'homepage#index'
 
   devise_for :students, controllers: {
     omniauth_callbacks: 'students/omniauth_callbacks'
   }
+  devise_for :employers
 
   resource :resume, controller: 'resume'
   resources :resumes
