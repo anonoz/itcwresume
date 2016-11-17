ef246388d24294fa4a33180d84128085d928dbb6
Anonoz Chong <honsiongchs@gmail.com>
2016-11-18 01:32:47 +0800

    Added prettier error pages

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,4 +1,13 @@
 class ErrorsController < ApplicationController
+  layout 'layouts/error'
+
   def not_found
+    @title = "404 Not Found - ITCW Resume"
+    render status: 404
+  end
+
+  def internal_server_error
+    @title = "500 Internal Server Error - ITCW Resume"
+    render status: 500
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,5 +24,6 @@ Rails.application.routes.draw do
     resources :employers
   end
 
-  get 'not_found' => 'errors#not_found'
+  match "/404", :to => "errors#not_found", :via => :all
+  match "/500", :to => "errors#internal_server_error", :via => :all
 end
