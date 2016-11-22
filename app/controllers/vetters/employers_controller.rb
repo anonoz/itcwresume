40d1b9c6d403d7e8d2bb9311a8969920740610fd
Anonoz Chong <honsiongchs@gmail.com>
2016-11-22 21:00:49 +0800

    Added New Employer form for vetters

--- app/controllers/vetters/employers_controller.rb
+++ app/controllers/vetters/employers_controller.rb
@@ -3,9 +3,22 @@ class Vetters::EmployersController < VettersController
     @employers = Employer.order("company_name ASC").all
   end
 
+  def new
+    @employer = Employer.new
+  end
+
+  def create
+    @employer = Employer.new(employer_params)
+    @employer.save!
+    redirect_to vetters_employers_path
+  end
+
   def edit
   end
 
   private
 
+  def employer_params
+    params.require(:employer).permit(:company_name, :email, :password)
+  end
 end
