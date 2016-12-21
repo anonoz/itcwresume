9df4aff86093ec46d0dc4945fadac1fc69b74355
Anonoz Chong <honsiongchs@gmail.com>
2016-12-21 15:24:57 +0800

    Added resume counter to employer navbar

--- app/controllers/employers_controller.rb
+++ app/controllers/employers_controller.rb
@@ -1,12 +1,18 @@
 class EmployersController < ApplicationController
   layout 'layouts/employer'
 
-  before_action :authenticate_employer!
+  before_action :authenticate_employer!, :set_navbar_resume_counts
 
   def contact_us
   end
 
   private
+
+  def set_navbar_resume_counts
+    inbox_count = Resume.for_employers.select("distinct student_id").count - Progress.where(company_id: current_company.id).select("distinct student_id").count
+    other_counts = Progress.group(:progress).select("distinct student_id").count
+    @navbar_resume_counts = other_counts.merge(0 => inbox_count)
+  end
 
   def current_company
     current_employer.company
