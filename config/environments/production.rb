6e0bb74a458f6ee4a9810f29b587fde4c4d75abf
dannyongtey <dannyongtey@gmail.com>
2017-06-17 18:51:22 +0800

    Update sendgrid domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,7 +102,7 @@ Rails.application.configure do
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
     password: ENV['SENDGRID_PASSWORD'],
-    domain: 'itcwresume.herokuapp.com',
+    domain: 'tcdresumeplatform.herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
     authentication: :plain,
