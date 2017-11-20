3bf41307190047a21d2f77ac96eff84441143cd2
dannyongtey <dannyongtey@gmail.com>
2017-11-20 16:28:56 +0800

    Add support for UTAR

--- app/models/student.rb
+++ app/models/student.rb
@@ -25,6 +25,8 @@
         student.university = "APU"
       when "kdu-online.com"
         student.university = "KDU"
+      when "1utar.my"
+        student.university = "UTAR"
       end
     end
   end
@@ -32,7 +34,7 @@
   private
 
   def email_from_uni
-    unless /@student.mmu.edu.my$/.match(email) or /@student.tarc.edu.my$/.match(email) or /@mail.apu.edu.my$/.match(email) or /@kdu-online.com$/.match(email)
+    unless /@student.mmu.edu.my$/.match(email) or /@student.tarc.edu.my$/.match(email) or /@mail.apu.edu.my$/.match(email) or /@kdu-online.com$/.match(email) or /@1utar.my$/.match(email)
       errors.add(:email, "must be gmail address from list of allowed domains!")
     end
   end
