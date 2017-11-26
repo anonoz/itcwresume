49842f8ba5910dd13202be09dea843507f6da107
dannyongtey <dannyongtey@gmail.com>
2017-11-26 22:29:14 +0800

    Improve Slackbot Notifier message

--- app/services/slackbot_notifier.rb
+++ app/services/slackbot_notifier.rb
@@ -13,17 +13,17 @@
 
   def notify_resume_upload(resume)
     send_message("#{resume.student.name} has uploaded a new resume "\
-      "(rev #{resume.reuploads}). #{Resume.latest_submissions.pending.count} pending. ")
+      "(rev #{resume.reuploads}). #{Resume.latest_submissions.pending.count} pending. #{Resume.latest_submissions.approved.count} currently approved. Vet now at https://resumetcd.herokuapp.com")
   end
 
   def notify_resume_approved(resume)
     send_message("#{resume.student.name}'s resume is approved. "\
-      "#{Resume.latest_submissions.pending.count - 1} left to go. ")
+      "#{Resume.latest_submissions.pending.count - 1} left to go. #{Resume.latest_submissions.approved.count} currently approved. Keep it going!")
   end
 
   def notify_resume_rejected(resume)
     send_message("#{resume.student.name}'s resume is rejected. "\
-      "#{Resume.latest_submissions.pending.count - 1} left to go. ")
+      "#{Resume.latest_submissions.pending.count - 1} left to go. #{Resume.latest_submissions.approved.count} currently approved.")
   end
 
   private
